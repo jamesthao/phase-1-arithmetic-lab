b8df29bd33076c9b7669292535784842b756c98c
James <jamesthao32@gmail.com>
2022-02-22 20:03:30 -0700

    commit tree ec969c057fb8cb1f6b14edece6f355108a46e55a

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 31
 
-// Write your code here
+let num2 = 2
+
+const multiply = num1 * num2
+
+let random = Math.floor(Math.random()* 10 + 1);
+
+console.log(random)
+
+let num3 = 16
+let num4 = 4
+const mod = num3 / num4
+
+const max = Math.max(1,2,3,4,20)
